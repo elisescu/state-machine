3b9dcb8f2c63cede13fba6286a53c3513fcb629a
Vasile Popescu <elisescu@elisescu.com>
2014-08-28 17:39:02 +0200

    use the message queue in the state machine
    - dequeue messages (which contain the next state) from the queue and
      block if there's no new message/state.
    - create new method used to set the new state where to transit (which
      will queue a new message with the new state).
    - create new method used to request the state machine to finish the
      execution.

--- state-machine.c
+++ state-machine.c
@@ -1,40 +1,96 @@
 #include "state-machine.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-#if 0
+#if 1
 #define LOGV(...) printf("\n" __VA_ARGS__)
 #else
 #define LOGV(...)
 #endif
 
-int run_machine(StateMachine_t *machine) {
+StateMachine_t* sm_create(char *name) {
+    StateMachine_t *machine = (StateMachine_t*) malloc(sizeof(StateMachine_t));
+    machine->machine_name = name;
+    machine->machine_thread = (pthread_t *) malloc(sizeof(pthread_t));
+    machine->machine_queue = mq_create();
+    machine->machine_mutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(machine->machine_mutex, NULL);
+    machine->machine_cond = (pthread_cond_t*) malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(machine->machine_cond, NULL);
+    machine->machine_running = 0;
+    return machine;
+}
+
+void* run_machine(void *data) {
+    StateMachine_t *machine = (StateMachine_t *) data;
     while (1) {
-        State_t *next_state = machine->next_state;
-        if (machine->next_state && machine->next_state->enter) {
-            LOGV(" entering to state %s", (machine->next_state->state_name) ? machine->next_state->state_name : "NULL");
-            if (machine->next_state->enter(machine, machine->next_state) == SM_EXIT) break;
+        pthread_mutex_lock(machine->machine_mutex);
+        int running = machine->machine_running;
+        pthread_mutex_unlock(machine->machine_mutex);
+        if (!running) {
+            LOGV(" machine running flag set to 0");
+            break;
+        }
+        Message_t *msg = mq_dequeue_message_wait(machine->machine_queue);
+        State_t *next_state = (State_t*) msg->data;
+        char *state_name = (next_state->state_name) ? next_state->state_name : "NULL";
+        if (next_state == NULL) {
+            LOGV(" no state left to run for the machine");
+            break;
+        }
+        if (next_state->enter) {
+            LOGV(" entering to state %s", state_name);
+            if (next_state->enter(machine, next_state) == SM_EXIT) {
+                LOGV(" machine exit normally after entering to the %s state", state_name);
+                break;
+            }
         }
 
-        machine->curr_state = machine->next_state;
-        if (machine->curr_state && machine->curr_state->run) {
-            LOGV(" running state %s", (machine->curr_state && machine->curr_state->state_name) ? machine->curr_state->state_name : "NULL");
-            if (machine->curr_state->run(machine, machine->curr_state) == SM_EXIT) break;
+        if (next_state->run) {
+            LOGV(" running state %s", state_name);
+            if (next_state->run(machine, next_state) == SM_EXIT) {
+                LOGV(" machine exit normally after running the %s state", state_name);
+                break;
+            }
         } else {
-            /* no state left to run
-             */
-            return SM_EXIT;
-        }
-
-        if (next_state == machine->next_state) {
-            LOGV("Looping in the same state (%s) ?", machine->next_state->state_name);
+            LOGV(" exiting state machine %s from state %s", machine->machine_name, state_name);
             break;
         }
 
-        if (machine->curr_state && machine->curr_state->exit) {
-            LOGV(" exiting state %s", (machine->curr_state && machine->curr_state->state_name) ? machine->curr_state->state_name : "NULL");
-            if (machine->curr_state->exit(machine, machine->curr_state) == SM_EXIT) break;
+        if (next_state->exit) {
+            LOGV(" exiting state %s", state_name);
+            if (next_state->exit(machine, next_state) == SM_EXIT) {
+                LOGV(" machine exit normally after exiting from the %s state", state_name);
+                break;
+            }
         }
     }
+    pthread_mutex_lock(machine->machine_mutex);
+    pthread_cond_signal(machine->machine_cond);
+    pthread_mutex_unlock(machine->machine_mutex);
+    LOGV(" main loop of the machine finished.");
+    return NULL;
+}
 
-    return SM_OK;
+void start_machine(StateMachine_t *machine) {
+    machine->machine_running = 1;
+    pthread_create(machine->machine_thread, NULL, run_machine, (void*)machine);
+}
+
+void sm_transit_to(StateMachine_t *sm, State_t *new_state) {
+    Message_t *msg = mq_message_create();
+    msg->data = (void*) new_state;
+    mq_queue_message(sm->machine_queue, msg);
+}
+
+void sm_finish(StateMachine_t *machine, int wait) {
+    LOGV("Finishing the state machine.");
+    pthread_mutex_lock(machine->machine_mutex);
+    machine->machine_running = 0;
+    if (wait) {
+        LOGV("Waiting for the machine to finish");
+        pthread_cond_wait(machine->machine_cond, machine->machine_mutex);
+        LOGV("Machine finshed execution!");
+    }
+    pthread_mutex_unlock(machine->machine_mutex);
 }
--- state-machine.h
+++ state-machine.h
@@ -1,6 +1,9 @@
 #ifndef STATEMACHINE_H
 #define STATEMACHINE_H
 
+#include "message-queue.h"
+#include <pthread.h>
+
 enum {
     SM_OK = 0,
     SM_EXIT = 1,
@@ -15,29 +18,59 @@ typedef int (*StateExitFunction_T)(struct StateMachine_s *, struct State_s *);
 typedef int (*StateRunFunction_T)(struct StateMachine_s *, struct State_s *);
 
 typedef struct State_s {
+    /**
+     * @brief enter is the function to execute when entering to the state. Can be NULL.
+     * Return SM_EXIT to stop the state machine.
+     */
     StateEnterFunction_T enter;
+    /**
+     * @brief run is the state function to be executed. Cannot be NULL. If NULL the state machine will stop.
+     * Return SM_EXIT to stop the state machine.
+     */
     StateRunFunction_T run;
+    /**
+     * @brief exit is the function to execute when exiting from the current state. Can be NULL.
+     * Return SM_EXIT to stop the state machine.
+     */
     StateExitFunction_T exit;
     char *state_name;
 } State_t;
 
 typedef struct StateMachine_s {
-    /**
-     * @brief next_state points to the next state of the machine. This should be set in the
-     * run function of the current state.
-     */
-
-    State_t *next_state;
-    /**
-     * @brief curr_state is the current state of the machine.
-     */
-    State_t *curr_state;
-    int no_states;
-    int run;
     char *machine_name;
+    MessageQueue_t *machine_queue;
+    pthread_t *machine_thread;
+    int machine_running;
+    pthread_cond_t *machine_cond;
+    pthread_mutex_t *machine_mutex;
 } StateMachine_t;
 
+/**
+ * @brief sm_create creates a new state machine with a given name.
+ * @param name the name of the state machine.
+ * @return  a pointer to the newly created state machine.
+ */
+StateMachine_t* sm_create(char *name);
 
-int run_machine(StateMachine_t *machine);
+/**
+ * @brief starts the state machine which will wait for new states to be queued in order to transit to them.
+ * @param machine the machine to start.
+ */
+void start_machine(StateMachine_t *machine);
+
+/**
+ * @brief sm_transit_to adds a new state to the states queue and notifies the state machine to run
+ * the new state.
+ * @param sm the state machine.
+ * @param new_state the new state to transit to.
+ */
+void sm_transit_to(StateMachine_t *sm, State_t *new_state);
+
+/**
+ * @brief sm_finish finish the state machine execution.
+ * @param machine the machine to finish
+ * @param wait 1 if we wish to wait until the machine has finished the execution.
+ */
+void sm_finish(StateMachine_t *machine, int wait);
 
 #endif // STATEMACHINE_H
