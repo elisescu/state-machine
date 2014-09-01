1a41ccdc3503c7cc5d94b9cd2c71f1ee9bb02167
Vasile Popescu <elisescu@elisescu.com>
2014-09-01 15:19:24 +0200

    update the state-machine test
    - use the new state-machine design.
    - test also messages sent to a state.
    - create a simple state machine with 3 states: s1, s2, s3 which
      will start in state s1 and then transit in state 2. Once being in
      s2 it will loop in s2 three times and then move to s3 after which
      it will be requested to finish. The main program will sleep
      for 2 seconds and then request the machine to finish.

--- test-sm.c
+++ test-sm.c
@@ -15,77 +15,107 @@ int state_trace_length = 0;
 State_t *s1, *s2, *s3;
 
 /****************** S1 state ******************/
-int s1_enter(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]='e';
-    return SM_OK;
+void s1_enter(StateMachine_t *m, State_t *s) {
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = 'e';
 }
-int s1_run(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]=s->state_name[0];
-    return SM_OK;
+void s1_process_message(StateMachine_t *m, State_t *s, StateMessage_t *msg) {
+    char *string_msg = msg->obj;
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = string_msg[0];
 }
-int s1_exit(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]='x';
-    return SM_OK;
+void s1_exit(StateMachine_t *m, State_t *s) {
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = 'x';
 }
 
 /****************** S2 state ******************/
-int s2_enter(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]='e';
-    return SM_OK;
+void s2_enter(StateMachine_t *m, State_t *s) {
+    static int counter = 0;
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = 'e';
+    if (counter++ < 3) {
+        sm_transit_to(m, s2);
+    } else {
+        sm_transit_to(m, s3);
+    }
 }
-int s2_run(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]=s->state_name[0];
-    return SM_OK;
+void s2_process_message(StateMachine_t *m, State_t *s, StateMessage_t *msg) {
+    char *string_msg = msg->obj;
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = string_msg[0];
 }
-int s2_exit(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]='x';
-    sm_transit_to(m, s3);
-    return SM_OK;
+void s2_exit(StateMachine_t *m, State_t *s) {
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = 'x';
 }
 
 /****************** S3 state ******************/
-int s3_enter(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]='e';
-    return SM_OK;
+void s3_enter(StateMachine_t *m, State_t *s) {
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = 'e';
 }
-int s3_run(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]=s->state_name[0];
-    return SM_OK;
+void s3_process_message(StateMachine_t *m, State_t *s, StateMessage_t *msg) {
+    char *string_msg = msg->obj;
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = string_msg[0];
 }
-int s3_exit(StateMachine_t *m, State_t *s) {
-    state_trace[state_trace_length++]='x';
-    return SM_EXIT;
+void s3_exit(StateMachine_t *m, State_t *s) {
+    state_trace[state_trace_length++] = s->state_name[0];
+    state_trace[state_trace_length++] = 'x';
 }
 
 int test_1() {
-    State_t s1_s = {s1_enter, s1_run, s1_exit, "1"};
-    State_t s2_s = {s2_enter, s2_run, s2_exit, "2"};
-    State_t s3_s = {s3_enter, s3_run, s3_exit, "3"};
+    State_t s1_s = {s1_enter, s1_process_message, s1_exit, "1"};
+    State_t s2_s = {s2_enter, s2_process_message, s2_exit, "2"};
+    State_t s3_s = {s3_enter, s3_process_message, s3_exit, "3"};
     s1 = &s1_s;
     s2 = &s2_s;
     s3 = &s3_s;
-    char *expected_result = "e1xe2xe3x";
+    char *expected_result = "1e1*1#1x2e2x2e2x2e2x2e2x3e3-3x";
 
     StateMachine_t *sm = sm_create("cool-state-machine");
 
+    // start the machine in STATE s1
     LOGV("Starting the state machine");
-    start_machine(sm);
-    sm_transit_to(sm, &s1_s);
-    sm_transit_to(sm, &s2_s);
+    start_machine(sm, s1);
+
+
+    // sending 2 messages to the machine
+    StateMessage_t msg;
+    msg.message_type = 3;
+    msg.obj = (void*)"*";
+    sm_send_state_message(sm, &msg);
+    StateMessage_t msg2;
+    msg2.message_type = 3;
+    msg2.obj = (void*)"#";
+    sm_send_state_message(sm, &msg2);
+
+    // transit to STATE s2
+    sm_transit_to(sm, s2);
 
     LOGV("Sleeping for a while now.");
     sleep(2);
     LOGV("Slept 2 seconds.");
 
+    // here we should already be in STATE s3
+    // send another message to the machine
+    StateMessage_t msg3;
+    msg3.message_type = 3;
+    msg3.obj = (void *) "-";
+    sm_send_state_message(sm, &msg3);
+
+
     sm_finish(sm, 1);
     state_trace[state_trace_length] = '\0';
     if (strcmp(state_trace, expected_result) == 0) {
         LOGV("Test passed! --> OK");
+        LOGV("State machine trace: %s ", state_trace);
         return 1;
     } else {
         LOGV("Test failed! --> ERROR");
-        LOGV("result: %s", state_trace);
-        LOGV("result: %s", expected_result);
+        LOGV("actual result:   %s", state_trace);
+        LOGV("expected result: %s", expected_result);
     }
     return 0;
 }
